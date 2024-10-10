4c3a50cb152d3c585ac7524193f0f9e9bbb06cbf
Timothy Lim <contacttimothylim@gmail.com>
2024-10-10 11:51:50 -0400

    connect to db then server

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ require('dotenv').config();
 
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -53,5 +48,13 @@ app.use((err, req, res, next) => {
   });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
