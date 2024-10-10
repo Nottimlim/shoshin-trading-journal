2bbc0348c1b0b7828b3654af000ee558566d00b3
Timothy Lim <contacttimothylim@gmail.com>
2024-10-10 11:34:06 -0400

    deploy

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/User.js');
 
 exports.getLoginPage = (req, res) => {
   if (req.session.user) {
